c4e5f3dd4a559419ecd384fa4982101e1900e0c9
kodcuserkan <akmanserkan65@gmail.com>
2019-10-31 02:14:31 +0300

    first commit

---
+++ src/index.js
@@ -0,0 +1,76 @@
+import React, { Component } from 'react'
+import './assets/style.css'
+import ReactDOM from 'react-dom'
+import quizService from './quizService'
+import QuestionBox from './components/QuestionBox'
+import Result from './components/Result'
+
+class QuizBee extends Component {
+    state = {
+        questionBank : [],
+        score : 0 ,
+        responses : 0
+    };
+    getQuestion = () => {
+        quizService().then(question => {
+            this.setState({
+                questionBank : question
+            })
+        })
+    }
+    componentDidMount(){
+        this.getQuestion();
+    }
+    computeAnswer = (answer , correctAnswer) => {
+        if (answer === correctAnswer) {
+            this.setState ({
+                score : this.state.score + 1
+            })
+        }
+      
+        this.setState ({
+            responses : this.state.responses < 5 ? this.state.responses + 1 : 5
+        })
+        console.log("Skor : ",this.state.score,"Responses : ",this.state.responses+1, "Answer : ", answer, "          Correct Answer : ",correctAnswer);
+
+    }
+    playAgain = () => {
+        this.getQuestion();
+        this.setState({
+            score : 0,
+            responses : 0
+        })
+    }
+    render() {
+        return (
+            
+            <div className="container">
+                <div className="title">Sınav Uygulaması</div>
+                {
+                    this.state.questionBank.length > 0 &&
+                    this.state.responses < 5 && 
+                    this.state.questionBank.map (
+                        ({question,answers,correct,questionId}) => 
+                        (
+                            <QuestionBox
+                                question = {question}
+                                options = {answers}
+                                key = {questionId}
+                                selected = {answer => this.computeAnswer(answer,correct)}
+                            />
+                            
+                        )
+                    )
+                }
+
+                {
+                    this.state.responses === 5 ? <Result score={this.state.score} playAgain={this.playAgain}/> : null
+                }
+            </div>
+
+        )
+    }
+}
+ReactDOM.render(<QuizBee/>, document.getElementById("root"));
+
+export default QuizBee;
